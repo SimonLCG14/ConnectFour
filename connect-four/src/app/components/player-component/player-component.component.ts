52ff91b404ee0879b5d077d3cbd4fdbd241b233b
Simon Matulin <s.matulin@students.htl-leonding.ac.at>
2025-11-27 14:18:18 +0100

    added player fields and box shadow

--- connect-four/src/app/components/player-component/player-component.component.ts
+++ connect-four/src/app/components/player-component/player-component.component.ts
@@ -1,4 +1,5 @@
-import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, OnInit, signal} from '@angular/core';
+import {Disk} from '../../model/disk';
 
 @Component({
   selector: 'app-player-component',
@@ -7,9 +8,21 @@ import {ChangeDetectionStrategy, Component, input} from '@angular/core';
   styleUrl: './player-component.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    '[style.--player-color]': 'color()',
+    '[style.--player-color]': 'color()'
   },
 })
-export class PlayerComponentComponent {
+export class PlayerComponentComponent implements OnInit{
+  private readonly AMOUNT_OF_DISKS: number = 21;
+
   color = input<string>('#f54542');
+  hasTurn = input<boolean>(true);
+  playerName = input<string>('No name');
+
+  remainingDisks = signal<Disk[]>([]);
+
+  ngOnInit() {
+    for (let i = 0; i < this.AMOUNT_OF_DISKS; i++){
+      this.remainingDisks().push({id: i, color: this.color()});
+    }
+  }
 }
